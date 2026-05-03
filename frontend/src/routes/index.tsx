316debd45e6bab78d2eac591ae525881e07bf846
ShikiHTM <kizukusan123@gmail.com>
2026-05-04 06:31:57 +0700

    feat (route): AppRoutes now an array instead of single object

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -2,10 +2,12 @@ import { type RouteObject } from "react-router-dom";
 import { authRoutes } from "../pages/auth/auth.routes";
 import Layout from "../layout";
 
-export const AppRoutes: RouteObject = {
-    'path': '/',
-    'element': <Layout />,
-    'children': [
-        ...authRoutes
-    ]
-}
+export const AppRoutes: RouteObject[] = [
+    {
+        'path': '/',
+        'element': <Layout />,
+        'children': [
+            ...authRoutes
+        ]
+    }
+]
