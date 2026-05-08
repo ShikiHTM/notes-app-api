759cde6d5bb784861e25272eb388e10e6ac26fb9
ShikiHTM <kizukusan123@gmail.com>
2026-05-08 21:22:31 +0700

    fix (Axios): change BACKEND_API_URL to VITE_API_URL

--- frontend/src/api/Axios.ts
+++ frontend/src/api/Axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.BACKEND_API_URL || 'https://api.shikii.dev/api/v1',
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         'Accept': 'application/json',
     }
