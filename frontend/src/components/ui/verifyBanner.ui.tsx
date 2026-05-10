2a800c98a696f2ac988a79f2d5945d4551bebb7f
ShikiHTM <kizukusan123@gmail.com>
2026-05-11 00:21:04 +0700

    feat: add verifyBanner to reminder user verify their account

---
+++ frontend/src/components/ui/verifyBanner.ui.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { PiWarningFill } from "react-icons/pi";
+import { useAuth } from "../../hooks/Auth.hook";
+
+const VerifyBanner: React.FC = () => {
+    const { user } = useAuth();
+    const [isLoading, setIsLoading] = useState(false);
+
+    return (
+        <div className={`w-full min-h-10 flex justify-center items-center bg-amber-400 text-white dark:bg-gh-attention ${user?.email_verified_at ? 'hidden' : ''}`}>
+            <div className="flex gap-2">
+                {/* Logo */}
+                <PiWarningFill size={24} />
+
+                <p className="font-medium">Vui lòng xác minh địa chỉ email của bạn tại <button
+                    className="text-blue-600 font-medium hover:text-blue-900 bg-transparent underline cursor-pointer"
+                    onClick={() => {
+                        setIsLoading(true);
+                    }}
+                >đây
+                </button></p>
+            </div>
+        </div>
+    )
+}
+
+export default VerifyBanner
