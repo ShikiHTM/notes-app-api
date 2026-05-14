9ff109a94c8d333db30a1db09c7a667248e9dd40
ShikiHTM <kizukusan123@gmail.com>
2026-05-14 17:35:48 +0700

    chore: add trailing newline to .gitignore and normalize quotes in Axios.ts

--- frontend/src/api/Axios.ts
+++ frontend/src/api/Axios.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+console.log(import.meta.env.VITE_API_URL);
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL + '/api/v1',
+    baseURL: import.meta.env.VITE_API_URL + "/api/v1",
     headers: {
         Accept: "application/json",
     },
